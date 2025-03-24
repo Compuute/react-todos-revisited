da29ca6ebf89850a2530862fbd1df9dbb157d836
Compuute <160307775+Compuute@users.noreply.github.com>
2025-03-24 20:59:14 +0100

    Add TodoList functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,9 +7,53 @@ import {
   deleteTodo,
   toggleTodoStatus,
 } from "./services/blockchainService";
+import TodoForm from "./components/TodoForm";
 
 function App() {
-  return <></>;
+  const [todos, setTodos] = useState([]);
+
+  const loadTodos = async () => {
+    const loadedTodos = await getTodos();
+    setTodos(loadedTodos);
+  };
+
+  useEffect(() => {
+    const init = async () => {
+      await initBlockchain();
+      await loadTodos();
+    };
+    init();
+  }, []);
+
+  const handleAddTodo = async (text) => {
+    await addTodo(text);
+    await loadTodos();
+  };
+
+  return (
+    <div className="App">
+      <h1>Todo List</h1>
+      <TodoForm onAddTodo={handleAddTodo} />
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <span
+              onClick={() => toggleTodoStatus(todo.id).then(loadTodos)}
+              style={{
+                textDecoration: todo.completed ? "line-through" : "none",
+                cursor: "pointer",
+              }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id).then(loadTodos)}>
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/services/blockchainService.js
+++ frontend/src/services/blockchainService.js
@@ -1,52 +1,56 @@
 import { ethers } from "ethers";
 import TodoList from "../contracts/TodoList.json";
 import contractAddress from "../contracts/contract-address.json";
+import TodoContract from "../contracts/TodoList.json";
 
 let contract;
 let signer;
 
 export const initBlockchain = async () => {
-    if(window.ethereum) {
-  const provider = new ethers.BrowserProvider(window.ethereum);
-  await provider.send("eth_requestAccounts", []);
-  signer = awaitprovider.getSigner();
-  contract = new ethers.Contract(contractAddress.TodoList, TodoContract.abi, signer);
-} else {
+  if (window.ethereum) {
+    const provider = new ethers.BrowserProvider(window.ethereum);
+    await provider.send("eth_requestAccounts", []);
+    signer = await provider.getSigner();
+    contract = new ethers.Contract(
+      contractAddress.TodoList,
+      TodoContract.abi,
+      signer
+    );
+  } else {
     alert("Please install MetaMask to use this app!");
-}
-}
+  }
+};
 //Hämta alla todos från kontraktet
 export const getTodos = async () => {
-    const todos = [];
-    const count = await contract.todoCount();
-    for (let i = 0; i < count; i++) {
-        const todo = await contract.todos(i);
-        if (todo.id.toString() !== "0" ) {
-            todos.push({
-                id: todo.id.toString(),
-                text: todo.text,
-                completed: todo.completed,
-            });
-        }
+  const todos = [];
+  const count = await contract.todoCount();
+  for (let i = 0; i < count; i++) {
+    const todo = await contract.todos(i);
+    if (todo.id.toString() !== "0") {
+      todos.push({
+        id: todo.id.toString(),
+        text: todo.text,
+        completed: todo.completed,
+      });
     }
-    return todos;
+  }
+  return todos;
 };
 
 //Lägg till en ny todo
 export const addTodo = async (text) => {
-    const tx = await contract.createTodo(text);
-    await tx.wait();
+  const tx = await contract.createTodo(text);
+  await tx.wait();
 };
-
 
 //Växla status: klar/ inte klar
 export const toggleTodoStatus = async (id) => {
-    const tx = await contract.toggleTodo(id);
-    await tx.wait();
+  const tx = await contract.toggleTodo(id);
+  await tx.wait();
 };
 
 //Ta bort en todo
 export const deleteTodo = async (id) => {
-    const tx = await contract.removeTodo(id);
-    await tx.wait();
+  const tx = await contract.removeTodo(id);
+  await tx.wait();
 };

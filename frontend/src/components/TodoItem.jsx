15578949b1d1e1ad5f979b0bd451fedd96ee8fec
Compuute <160307775+Compuute@users.noreply.github.com>
2025-03-24 21:04:43 +0100

    Added TodoItem functions

---
+++ frontend/src/components/TodoItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function TodoItem({ todo, onToggle, onDelete }) {
+    return (
+        <li>
+            <span
+                onClick={onToggle}
+                style={{
+                    textDecoration: todo.completed ? "line-through" : "none",
+                    cursor: "pointer",
+                
+                }}
+                >
+                    {todo.text}
+                </span>
+            <button onClick={onDelete}>Delete</button>
+        </li>
+    );
+}
+
+export default TodoItem;
